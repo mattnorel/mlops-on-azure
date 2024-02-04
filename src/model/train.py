ee6ed3c066f16f7a8df2de04604015e33fb24fe0
Mateusz Norel <imegirin@gmail.com>
2024-02-04 19:57:55 +0100

    implemented data split function

--- src/model/train.py
+++ src/model/train.py
@@ -4,6 +4,8 @@ import argparse
 import glob
 import os
 
+from sklearn.model_selection import train_test_split
+import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
@@ -11,8 +13,7 @@ from sklearn.linear_model import LogisticRegression
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -33,8 +34,9 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
-
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.3, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
